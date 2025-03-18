e20e6b3f1df5ec605a86a05f085b6ca57e926ccf
SydorPavlo <pavlo44957@gmail.com>
2025-03-18 17:51:40 +0200

    playable

--- script.js
+++ script.js
@@ -1,14 +1,62 @@
 const table = document.querySelector('.table');
+const ball = document.querySelector('.ball');
+ 
 const classes = [
   [['ru', 'ld'], ['rd', 'lu']],
-  [['ru2' ,'ld2'], ['rd2', 'lu2']]
-]
+  [['ru2', 'ld2'], ['rd2', 'lu2']]
+];
 
-// switchPlaces(0, 1);
+
+await placeBall();
+await shuffle(20);
 
 table.onclick = handleClick;
 
+async function shuffle(n) {
+  for (let i = 0; i < n; i++) {
+    const indices = [0, 1, 2];
+    indices.splice(rnd(3), 1);
+    console.log(indices);
+
+    await switchPlaces(...indices);
+  }
+}
+
+async function placeBall() {
+  const thimble = table.children[rnd(3)];
+
+  thimble.append(ball);
+
+  await new Promise(resolve => setTimeout(resolve, 500));
+
+  thimble.classList.add('open');
+
+  await Promise.all([
+    new Promise(resolve => thimble.ontransitionend = resolve),
+    new Promise(resolve => ball.ontransitionend = resolve)
+  ]);
+
+  thimble.classList.remove('open');
+
+  return Promise.all([
+    new Promise(resolve => ball.ontransitionend = resolve),
+    new Promise(resolve => thimble.ontransitionend = resolve)
+  ])
+}
+
 async function handleClick(e) {
+  const thimble = e.target.closest('.thimble');
+
+  if (!thimble) return
+
+  thimble.classList.add('open');
+
+  await new Promise(resolve => thimble.ontransitionend = resolve);
+
+  thimble.classList.remove('open');
+}
+
+async function handleSelect(e) {
   const thimble = e.target.closest('.thimble');
 
   if (!thimble || table.matches('.busy')) return;
@@ -46,20 +94,19 @@ async function switchPlaces(i1, i2) {
   thimble1.classList.add(class1);
   thimble2.classList.add(class2);
 
-  return new Promise(resolve => {
-    thimble1.onanimationend = () => {
-      thimble1.classList.remove(class1);
-      thimble2.classList.remove(class2);
+  await Promise.all([
+    new Promise(resolve => thimble1.onanimationend = resolve),
+    new Promise(resolve => thimble2.onanimationend = resolve)
+  ]);
 
-      const div = document.createElement('div');
+  thimble1.classList.remove(class1);
+  thimble2.classList.remove(class2);
 
-      thimble1.replaceWith(div);
-      thimble2.replaceWith(thimble1);
-      div.replaceWith(thimble2);
+  const div = document.createElement('div');
 
-      resolve();
-    }
-  });
+  thimble1.replaceWith(div);
+  thimble2.replaceWith(thimble1);
+  div.replaceWith(thimble2);
 }
 
 function rnd(max) {
